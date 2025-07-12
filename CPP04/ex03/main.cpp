7b51cb7462fc76928e4a6c9351786d51b3718d3d
tac <tac@tacnoMacBook-Air.local>
2025-07-12 13:41:48 +0900

    test

--- CPP04/ex03/main.cpp
+++ CPP04/ex03/main.cpp
@@ -11,21 +11,50 @@ int main()
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
 	
-	ICharacter* me = new Character("me");
+	ICharacter* me = new Character("Alice");
+	ICharacter* bob = new Character("Bob");
+	ICharacter* charlie = new Character("Charlie");
 	
 	AMateria* tmp;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
 	
-	ICharacter* bob = new Character("bob");
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
 	
 	me->use(0, *bob);
 	me->use(1, *bob);
+	me->use(2, *charlie);
+	me->use(3, *charlie);
+	me->use(4, *bob);
 	
+	me->unequip(1);
+	me->use(1, *bob);
+	
+	tmp = src->createMateria("lightning");
+	if (tmp)
+		me->equip(tmp);
+	
+	Character* copy = new Character(*static_cast<Character*>(me));
+	copy->use(0, *bob);
+	
+	Character assigned("Assigned");
+	assigned = *static_cast<Character*>(me);
+	assigned.use(2, *charlie);
+	
+	me->unequip(0);
+	copy->use(0, *bob);
+	
+	delete charlie;
 	delete bob;
 	delete me;
+	delete copy;
 	delete src;
 	
 	return 0;
